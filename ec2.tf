0b54f34b021228422a266adc496f2319f67f8d6b
Oleksii Bebych <oleksii.bebych@gmail.com>
2024-03-03 13:03:27 +0200

    added tfsec

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 variable "subnet_id" {
-  type        = string
+  type = string
 }
 
 data "aws_ami" "amzn-linux-2023-ami" {
@@ -16,6 +16,17 @@ resource "aws_instance" "example" {
   ami           = data.aws_ami.amzn-linux-2023-ami.id
   instance_type = "t3.small"
   subnet_id     = var.subnet_id
+
+  root_block_device {
+    volume_size           = "20"
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  metadata_options {
+    http_tokens = "required"
+  }
 
   tags = {
     Name = "tf-example"
