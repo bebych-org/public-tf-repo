bb90eab97b0f55006277ecf5901a8bfdb6c8dc1d
Oleksii Bebych <oleksiibebych@Oleksiis-MacBook-Pro.local>
2024-03-03 12:32:35 +0200

    initial commit ec2

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+variable "subnet_id" {
+  type        = string
+}
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "t3.small"
+  subnet_id     = var.subnet_id
+
+  tags = {
+    Name = "tf-example"
+  }
+}
